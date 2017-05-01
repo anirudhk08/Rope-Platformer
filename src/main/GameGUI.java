93f95ab3c7b929f157f7051020be15f9c9a94827
Aubhro Sengupta <aubhrosengupta@gmail.com>
2017-05-01 10:21:24 -0700

    Add base files

---
+++ src/main/GameGUI.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameGui extends Application
+{
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
