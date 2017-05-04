9d8cdf61753ab59665e1121f1cba5822a933e8bd
Aubhro Sengupta <aubhrosengupta@gmail.com>
2017-05-04 09:45:04 -0700

    Add start() to GameGUI and fix all errors

--- src/main/GameGUI.java
+++ src/main/GameGUI.java
@@ -1,10 +1,18 @@
+package main;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class GameGui extends Application
+public class GameGUI extends Application
 {
 	public static void main(String[] args)
 	{
 		launch(args);
 	}
+
+    public void start(Stage s)
+    {
+        s.setTitle("Hello, World!");
+        s.show();
+    }
 }
