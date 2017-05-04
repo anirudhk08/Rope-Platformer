645bba850aa891cee4acdf67925208d00442c206
anirudhk08 <Anirudh@Anirudhs-MacBook-Pro-2.local>
2017-05-04 09:46:42 -0700

    Add MainMethod

--- src/MainMenu/MainMenu.java
+++ src/MainMenu/MainMenu.java
@@ -1,8 +1,36 @@
 package MainMenu;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by Anirudh on 5/4/17.
  */
-public class MainMenu {
-    
+public class MainMenu extends JFrame implements ActionListener {
+    private JMenuItem item1 = new JMenuItem("Open");
+    private JMenuItem item2 = new JMenuItem("Cancel");
+
+    public MainMenu()
+    {
+        JFrame F = new JFrame("ismail");
+        JMenuBar menubar = new JMenuBar();
+        JMenu menu1 = new JMenu("File");
+
+        item1.addActionListener(this);
+        item2.addActionListener(this);
+
+        menu1.add(item1);
+        menu1.add(item2);
+
+
+        menubar.add(menu1);
+        F.setJMenuBar(menubar);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
