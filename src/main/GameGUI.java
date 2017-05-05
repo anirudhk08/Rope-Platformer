3d2b1176f31a4ccc044a96c09b6274b560de1264
anirudhk08 <anirudhk08@gmail.com>
2017-05-05 10:24:35 -0700

    create/update new classes

---
+++ src/main/GameGUI.java
@@ -0,0 +1,21 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Anirudh
+ */
+public class GameGUI extends Application
+{
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
+    public void start(Stage s)
+    {
+        s.setTitle("Hello, World!");
+        s.show();
+    }
+}
