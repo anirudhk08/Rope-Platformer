d0f20897238ee0859b4423a68154433261495e73
Aubhro Sengupta <aubhrosengupta@gmail.com>
2017-05-26 10:26:34 -0700

    Add main directory

--- src/data/StickFigureAnimations.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-/**
- * Created by axu047 on 5/23/2017.
- */
-public class StickFigureAnimations {
-
-
-}
--- src/gui/Settings.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-import javax.swing.*;
-
-/**
- * Created by axu047 on 5/11/2017.
- */
-public class Settings extends JPanel {
-}
