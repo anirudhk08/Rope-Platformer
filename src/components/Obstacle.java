ebf250509597042d3a001b388a56f1b6d3ca9696
anirudhk08 <anirudhk08@gmail.com>
2017-05-28 22:10:28 -0700

    Documentation of components package, classes and methods

--- src/components/Obstacle.java
+++ src/components/Obstacle.java
@@ -8,16 +8,42 @@ import static menus.GameLevelConstants.*;
 import static menus.GameWindowConstants.WINDOW_HEIGHT;
 import static menus.GameWindowConstants.WINDOW_WIDTH;
 
-/**
- * Created by Anirudh on 5/27/2017.
- */
-public class Obstacle extends GameComponent {
 
+/**
+ * Class representing Obstacle game component. These Obstacles blocks
+ * the stick figure from moving forward and hence considered harmful.
+ *
+ * This class provide methods to create and manage Obstacles for
+ * different levels of the game.
+ *
+ */
+public class Obstacle extends GameComponent
+{
+
+    /**
+     * Color of the obstacle
+     */
     private Color color;
+
+    /**
+     * Flag to indicate if obstacle can move or not
+     */
     private boolean move;
 
 
-    public Obstacle(double startX, double startY, Color color, int width, int height, boolean move) {
+    /**
+     * Constructor to initialize obstacle with its coordinates, widhth, height, color
+     * and if it can move in random direction.
+     *
+     * @param startX - center x coordinate of the obstacle
+     * @param startY - center y coordinate of the obstacle
+     * @param color - color of the obstacle object
+     * @param width - width of the obstacle object
+     * @param height - height of the obstacle object
+     * @param move - indicates if the obstacle can move or not
+     */
+    public Obstacle(double startX, double startY, Color color, int width, int height, boolean move)
+    {
         super(startX, startY, width, height);
 
         this.color = color;
@@ -26,6 +52,12 @@ public class Obstacle extends GameComponent {
     }
 
 
+    /**
+     * Method to create a list of obstacles based a game level.
+     *
+     * @param gameLevel - parameter to indicate game level
+     * @return - list of obstacles
+     */
     public static ArrayList<Obstacle> createObstacles(int gameLevel)
     {
         ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>();
@@ -57,11 +89,21 @@ public class Obstacle extends GameComponent {
         return obstacles;
     }
 
-
-    private static void createIntroObstacles(ArrayList<Obstacle> obstacles) {
+    /**
+     * Method to create obstacle for "Introduction" game level
+     *
+     * @param obstacles - obstacle list
+     */
+    private static void createIntroObstacles(ArrayList<Obstacle> obstacles)
+    {
         obstacles.add(new Obstacle(500, 50, Color.BLUE, 150, 75, true));
     }
 
+    /**
+     * Method to create obstacle for "Good Day" game level
+     *
+     * @param obstacles - obstacle list
+     */
     private static void createGoodObstacles(ArrayList<Obstacle> obstacles) {
 
         obstacles.add(new Obstacle(100, 620, Color.BLUE, 150, 75, true));
@@ -70,6 +112,11 @@ public class Obstacle extends GameComponent {
 
     }
 
+    /**
+     * Method to create obstacle for "Cool Day" game level
+     *
+     * @param obstacles - obstacle list
+     */
     private static void createCoolObstacles(ArrayList<Obstacle> obstacles)
     {
         obstacles.add(new Obstacle(100, 620, Color.BLUE, 75, 75, true));
@@ -79,6 +126,11 @@ public class Obstacle extends GameComponent {
 
     }
 
+    /**
+     * Method to create obstacle for "Scary Day" game level
+     *
+     * @param obstacles - obstacle list
+     */
     private static void createScaryObstacles(ArrayList<Obstacle> obstacles)
     {
         obstacles.add(new Obstacle(100, 620, Color.BLUE, 150, 75, true));
@@ -88,6 +140,11 @@ public class Obstacle extends GameComponent {
     }
 
 
+    /**
+     * Method to create obstacle for "Unbearable Day" game level
+     *
+     * @param obstacles - obstacle list
+     */
     private static void createUnbearableObstacles(ArrayList<Obstacle> obstacles)
     {
         obstacles.add(new Obstacle(100, 100, Color.YELLOW, 70, 60, true));
@@ -96,6 +153,12 @@ public class Obstacle extends GameComponent {
         obstacles.add(new Obstacle(640, 200, Color.ORANGE, 50, 50, true));
         obstacles.add(new Obstacle(800, 540, Color.CYAN, 90, 90, true));
     }
+
+    /**
+     * Method to create obstacle for "Final Day" game level
+     *
+     * @param obstacles - obstacle list
+     */
     private static void createFinalObstacles(ArrayList<Obstacle> obstacles)
     {
         obstacles.add(new Obstacle(200, 100, Color.CYAN, 50, 50, true));
@@ -108,16 +171,40 @@ public class Obstacle extends GameComponent {
     }
 
 
-    public boolean canMove() {
+    /**
+     * Method to know if this game component can move
+     * in random directions
+     *
+     * @return - true if game component can move
+     *    false if game component can move
+     */
+    public boolean canMove()
+    {
         return move;
     }
 
+    /**
+     * Method to generate either negative or postive random numbers
+     *
+     * @return - negative or positive randam number
+     */
     private int getRandomDirection()
     {
-        int rand =  new Random().nextInt(7) - 3;
+        int max = 4;
+        int min = -4;
+
+        int rand =  new Random().nextInt(max - min + 1) - max;
+
         return rand;
     }
 
+    /**
+     * Method to update the position of obstacle in a random direction
+     *
+     * The sign of x-coordinate and y-coordinate determines
+     * the directions: (+, +), (+, -), (-, +), (-, -)
+     *
+     */
     public void updatePosition()
     {
         if (move)
@@ -140,12 +227,23 @@ public class Obstacle extends GameComponent {
 
     }
 
+    /**
+     * Method to move the obstacle to specified position
+     *
+     * @param xPos - center x coordinate of obstacle
+     * @param yPos - center y coordinate of obstacle
+     */
     public void updatePosition(double xPos, double yPos)
     {
         this.xPos = xPos;
         this.yPos = yPos;
     }
 
+    /**
+     * Method to draw the the obstacle
+     * 
+     * @param g - graphics object
+     */
     @Override
     public void draw(Graphics2D g)
     {
--- src/components/Player.java
+++ src/components/Player.java
@@ -13,63 +13,131 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 /**
- * Created by axu047 on 5/5/2017.
+ * The Player class controls a StickFigure (and thus it uses a reference
+ * to one). More specifically, it is a KeyListener and MouseListener that
+ * uses such input methods to control the StickFigure. These actions will
+ * be registered in KeyBinding’s input() method, which will return a lambda
+ * that is a method of StickFigure; this method will then be invoked.
+ *
  */
-public class Player implements KeyListener, MouseListener {
+public class Player implements KeyListener, MouseListener
+{
+    /**
+     * Stick figure object
+     */
     private StickFigure s;
+
+    /**
+     * Key settings
+     */
     private KeyBindings settings;
+
+    /**
+     * GUI panel
+     */
     private Game parent;
 
-    public Player(StickFigure stickFigure, KeyBindings keys, Game g) {
+    /**
+     * Constructor to initialize the Player instance with stick figure,
+     * key settings and GUI panel
+     *
+     * @param stickFigure
+     * @param keys
+     * @param g
+     */
+    public Player(StickFigure stickFigure, KeyBindings keys, Game g)
+    {
         s = stickFigure;
         settings = keys;
         parent = g;
     }
 
+    /**
+     * Method to process the Key typed event
+     * @param e - key event
+     */
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(KeyEvent e)
+    {
 
     }
 
+    /**
+     * Method to process the Key pressed event
+     * @param e - key event
+     */
     @Override
-    public void keyPressed(KeyEvent e) {
-        if (settings.get(e.getKeyCode()) == PlayerActions.EXIT) {
+    public void keyPressed(KeyEvent e)
+    {
+        if (settings.get(e.getKeyCode()) == PlayerActions.EXIT)
+        {
             parent.exit();
             return;
         }
+
         StickFigureAction action = settings.input(e, s);
         action.action();
     }
 
+    /** Method to process the Key released event
+     *
+     * @param e - key event
+     */
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased(KeyEvent e)
+    {
         StickFigureAction action = settings.release(e, s);
         action.action();
     }
 
+    /**
+     *  Method to process the mouse clicked event
+     *
+     * @param e - key event
+     */
     @Override
-    public void mouseClicked(MouseEvent e) {
+    public void mouseClicked(MouseEvent e)
+    {
 
     }
 
+    /**
+     * Method to process the mouse pressed event
+     *
+     * @param e - mouse event
+     */
     @Override
-    public void mousePressed(MouseEvent e) {
+    public void mousePressed(MouseEvent e)
+    {
         StickFigureActionCoordinates action = settings.click(e, s);
         action.action(e.getX(), e.getY());
     }
 
+    /**
+     * Method to process the mouse released event
+     *
+     * @param e - mouse event
+     */
     @Override
-    public void mouseReleased(MouseEvent e) {
+    public void mouseReleased(MouseEvent e)
+    {
 
     }
 
+    /**
+     * Method to process mouse entered event
+     *
+     * @param e - mouse event
+     */
     @Override
-    public void mouseEntered(MouseEvent e) {
+    public void mouseEntered(MouseEvent e)
+    {
         parent.requestFocus();
     }
 
     @Override
-    public void mouseExited(MouseEvent e) {
+    public void mouseExited(MouseEvent e)
+    {
 
     }
 }
